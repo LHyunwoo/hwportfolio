7addebef62f0e0812c3f9a58635ab7ebe707c78a
hyunwoo <gusdndls@onpoom.co.kr>
2024-04-03 10:54:15 +0900

    commit2

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,8 @@ const greeting = {
   username: "Hyeon Woo",
   title: "Hi all, I'm HyeonWoo",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    // "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "안녕하세요!!"
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
